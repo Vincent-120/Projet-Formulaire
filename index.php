03e341b40d49d3e422c2bac11f7ebb0af1e2b47b
Vincent <v1996.vincent@gmail.com>
2019-12-09 00:52:41 +0100

    mise en place du css

--- index.php
+++ index.php
@@ -18,59 +18,38 @@
 <a href="#contacte">Contacte</a>
 </nav>
 </header>
-<!--Ici premiere partie de la page-->
+<!--?*Ici premiere partie de la page-->
+<div id="fond">
     <div id="acceuille" class="acceuille container-fluid">
         <div class="row">
-            <div id="wrapper" class="titre col-sm-12 col-md-5 text-center order-2">
-    <h1>
-        <span id="title">
-            <span class="title-letter">H</span>
-            <span class="title-letter">a</span>
-            <span class="title-letter">c</span>
-            <span class="title-letter">k</span>
-            <span class="title-letter">e</span>
-            <span class="title-letter">u</span>
-            <span class="title-letter">r</span>
-            <span class="title-letter">s</span>
-            <span class="title-letter">&nbsp;</span>
-            <span class="title-letter">P</span>
-            <span class="title-letter">o</span>
-            <span class="title-letter">u</span>
-            <span class="title-letter">l</span>
-            <span class="title-letter">e</span>
-            <span class="title-letter">t</span>
-            <span class="title-letter">t</span>
-            <span class="title-letter">e</span>
-        </span>
-
-        <span id="sub-title">We Raspberry Pi</span>
-    </h1>
+            <div class="titre col-sm-12 col-md-5 text-center order-2">
                 <img class="logo" class="img"src="Assets/img/hackers-poulette-logo.png" alt="logo,text,image">
+                <h3 class="text-light">We Raspberry</h3>
             </div>
-            <div class="who col-sm-12 col-md-3 text-center shadow-lg">
+            <div class="who col-sm-12 col-md-3 text-center order-1 shadow-lg">
                 <div class="card-header">
-                    <h4 class="my-0 font-weight-normal">A propos</h4>
+                    <h4 class="my-0 font-weight-normal"><b>A propos</b></h4>
                 </div>
-                <div class="card-body">
+                <div class="card-body1">
                     <p><b>Hackers Poulette</b> est un site conçu pour les debutants, les fans et les veterans des rasberry ! Nous metons tout en oeuvre pour que nos client qui soit-il puisse obtenir ce nano-ordinateur et l'utiliser, le comprendre sur toute ses coutures. Nous offrons un commerce mondial afin que tout le monde ou qu'il soit puisse en obtenir un ! Fraichement née en 2017 <b>Hackers Poulette</b> ce lance dans la conquete du monde avec les Rasperry Pi avec ce nouveau site web.</p>
                     <a href="#produit"> Alors n'hésiter plus et venez voir nos produit !</a>
                 </div>
             </div>
-            <div class="produit col-sm-12 col-md-3 text-center order-3 shadow-lg">
+            <div class="produit col-sm-12 col-md-3 text-center shadow-lg order-3">
                 <div class="card-header">
-                    <h4 class="my-0 font-weight-normal">Produits</h4>
+                    <h4 class="my-0 font-weight-normal"><b>Produit</b></h4>
                 </div>
-                <div class="card-body">
+                <div class="card-body1">
                     <p> Le Raspberry Pi est un nano-ordinateur monocarte à processeur ARM conçu par des professeurs du département informatique de l'université de Cambridge dans le cadre de la fondation Raspberry Pi2. Cet ordinateur, de la taille d'une carte de crédit, est destiné à encourager l'apprentissage de la programmation informatique2. Il permet l'exécution de plusieurs variantes du système d'exploitation libre GNU/Linux, notamment Debian, et des logiciels compatibles. Mais il fonctionne également avec Windows 10 IoT Core3 et celui de Google, Android Pi4.</p>
                 </div>
             </div>
-            <div class="info col-sm-12 col-md-12 text-center order-4 shadow-lg">
+            <div class="info col-sm-12 col-md-12 text-center shadow-lg order-4">
                 <div class="card-header">
-                    <h4 class="my-0 font-weight-normal">Mais aussi !</h4>
+                    <h4 class="my-0 font-weight-normal"><b>Mais aussi !</b></h4>
                 </div>
-                <div class="card-body">
+                <div class="card-body1">
                     <p>Afin de permettre a tous d'utiliser un Rasperry Pi nous avons fait appel a different associiation qui vous permettrons d'apprendre a utiliser un Rasperry Pi ! Et oui beaucoup de gens sont intérresser par ce nano-ordinateur, pourquoi ? car il permet différente chose, tres interressant les une des autres ! Mais nous n'allons pas vous retirer ce mystere héhé. Nous vous laissons pousser votre creativité sur ce petit objet. </p>
-                    <ul class="list-unstyled mt-3 mb-4">
+                    <ul class="list-unstyled">
                         <li><a href="https://codeclub.org/en/">Code Club</a></li>
                         <li><a href="https://coderdojo.com/fr-FR">Coder Dojo</a></li>
                         <li><a href="https://www.raspberrypi.org/jam/">Rasperry Jam</a></li>
@@ -79,14 +58,15 @@
             </div>
         </div>
     </div>
-    <!--ici ce trouve la deuxieme partie de la page-->
-    <div class="pr-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
-        <h1 id="produit" class="display-4">Produits</h1>
-    <div class="container">
+</div>
+    <!--?*ici ce trouve la deuxieme partie de la page-->
+    <div class="fond2">
+<div class="container">
+    <h1 id="produit" class="col-md-12 text-center text-light"><b>Produits</b></h1>
         <div class="card-deck mb-3 text-center">
-            <div class="card mb-4 shadow-lg">
+            <div class="card mb-4 shadow-sm">
                 <div class="card-header">
-                    <h4 class="my-0 font-weight-normal">Raspberry Pi B</h4>
+                    <h4 class="my-0 font-weight-normal">Raspberry Pi Bs</h4>
                 </div>
                 <div class="card-body">
                     <img id="img" src="Assets/img/Raspberry_Pi_Model_B.jpg" alt="Raspberry Pi B">
@@ -95,89 +75,93 @@
                         <li>empty</li>
                         <li>empty</li>
                     </ul>
-                    <button type="button" class="btn btn-lg btn-block btn-outline-primary">70€</button>
+                    <button type="button" class="btn btn-lg btn-block btn-outline-primary"><b>Acheter</b></button>
                 </div>
             </div>
-            <div class="card mb-4 shadow-lg">
-                <div class="card-header">
-                    <h4 class="my-0 font-weight-normal">Raspberry PI B+</h4>
-                </div>
-                <div class="card-body">
-                    <img id="img" src="Assets/img/Raspberry_Pi_B+.jpg" alt="Raspberry PI B+">
-                    <ul class="list-unstyled mt-3 mb-4">
-                        <li>4 GB of storage</li>
-                        <li>empty</li>
-                        <li>empty</li>
-                    </ul>
-                    <button type="button" class="btn btn-lg btn-block btn-outline-primary">80€</button>
-                </div>
-            </div>
-            <div class="card mb-4 shadow-lg">
+        
+        <div class="card mb-4 shadow-sm">
             <div class="card-header">
-                <h4 class="my-0 font-weight-normal">Raspberry Pi 2</h4>
+                <h4 class="my-0 font-weight-normal">Raspberry PI B+</h4>
             </div>
             <div class="card-body">
+            <img id="img" src="Assets/img/Raspberry_Pi_B+.jpg" alt="Raspberry PI B+">
                 <ul class="list-unstyled mt-3 mb-4">
-                    <img id="img" src="Assets/img/Raspberry_Pi_2.jpg" alt="Raspberry Pi 2">
-                    <li>2 GB of storage</li>
+                    <li>10 GB of storage</li>
                     <li>empty</li>
                     <li>empty</li>
                 </ul>
-            <button type="button" class="btn btn-lg btn-block btn-outline-primary">98€</button>
+                <button type="button" class="btn btn-lg btn-block btn-outline-primary"><b>Acheter</b></button>
             </div>
+        </div>
+        <div class="card mb-4 shadow-sm">
+            <div class="card-header">
+                <h4 class="my-0 font-weight-normal">Raspberry Pi 2</h4>
+            </div>
+        <div class="card-body">
+        <img id="img" src="Assets/img/Raspberry_Pi_2.jpg" alt="Raspberry Pi 2">
+            <ul class="list-unstyled mt-3 mb-4">
+                <li>15 GB of storage</li>
+                <li>empty</li>
+                <li>empty</li>
+            </ul>
+            <button type="button" class="btn btn-lg btn-block btn-outline-primary"><b>Acheter</b></button>
         </div>
     </div>
     <div class="container">
         <div class="card-deck mb-3 text-center">
-            <div class="card mb-4 shadow-lg">
+            <div class="card mb-4 shadow-sm">
                 <div class="card-header">
-                    <h4 class="my-0 font-weight-normal">Raspberry Pi 3</h4>
+                    <h4 class="my-0 font-weight-normal">Raspberry Pi Bs</h4>
                 </div>
                 <div class="card-body">
-                    <img id="img" src="Assets/img/Raspberry_Pi_3.JPG" alt="Raspberry Pi 3">
+                <img id="img" src="Assets/img/Raspberry_Pi_3.JPG" alt="Raspberry Pi 3">
                     <ul class="list-unstyled mt-3 mb-4">
                         <li>2 GB of storage</li>
                         <li>empty</li>
                         <li>empty</li>
                     </ul>
-                    <button type="button" class="btn btn-lg btn-block btn-outline-primary">110€</button>
+                    <button type="button" class="btn btn-lg btn-block btn-outline-primary"><b>Acheter</b></button>
                 </div>
             </div>
-            <div class="card mb-4 shadow-lg">
+            <div class="card mb-4 shadow-sm">
                 <div class="card-header">
                     <h4 class="my-0 font-weight-normal">Raspberry Pi 3B+</h4>
                 </div>
                 <div class="card-body">
                 <img id="img" src="Assets/img/Raspberry_Pi_3_b+.jpg" alt="Raspberry Pi 3B+">
                     <ul class="list-unstyled mt-3 mb-4">
-                        <li>4 GB of storage</li>
+                        <li>10 GB of storage</li>
                         <li>empty</li>
                         <li>empty</li>
                     </ul>
-                    <button type="button" class="btn btn-lg btn-block btn-outline-primary">115€</button>
+                    <button type="button" class="btn btn-lg btn-block btn-outline-primary"><b>Acheter</b></button>
                 </div>
             </div>
-            <div class="card mb-4 shadow-lg">
-            <div class="card-header">
-                <h4 class="my-0 font-weight-normal">Raspberry Pi 4</h4>
-            </div>
+            <div class="card mb-4 shadow-sm">
+                <div class="card-header">
+                    <h4 class="my-0 font-weight-normal">Raspberry Pi 2</h4>
+                </div>
             <div class="card-body">
-                <img id="img" src="Assets/img/Raspberry_Pi_4.jpg" alt="Raspberry Pi 4">
+            <img id="img" src="Assets/img/Raspberry_Pi_4.jpg" alt="Raspberry Pi 4">
                 <ul class="list-unstyled mt-3 mb-4">
-                    <li>2 GB of storage</li>
+                    <li>15 GB of storage</li>
                     <li>empty</li>
                     <li>empty</li>
                 </ul>
-            <button type="button" class="btn btn-lg btn-block btn-outline-primary">135€</button>
+                <button type="button" class="btn btn-lg btn-block btn-outline-primary"><b>Acheter</b></button>
             </div>
         </div>
     </div>
+</div>
+</div>
+</div>
+</div>
     <!-- Ici ce trouve le formulaire-->
+    <div class="fond3">
     <div class="container-fluid">
         <div class="row">
-           <div class="col-md-2 "></div>
-            <form id="contacte"method="post" action="index.php" class="formulaire col-md-10 col-sm-12 text-center align-item-center">
-                <h1 class="com-md-12 text-center">Formulaire de contacte :</h1>
+            <form id="contacte"method="post" action="index.php" class="formulaire col-md-12 col-sm-12 text-center align-content-center">
+                <h1 class="com-md-12 text-center text-light"><b>Formulaire de contacte :</b></h1>
                 <div id="tv" >
                     <div class="col-sm-12 col-sm-offset-4" id="form">
                         <div class="well" style="margin-top: 10%;">
@@ -238,8 +222,8 @@
                     </div>
 
                 </div>
+            </div>
         </div>
-    </div>
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
